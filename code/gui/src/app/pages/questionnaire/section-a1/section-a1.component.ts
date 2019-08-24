7a083f8e12d1a489f1d56d3cbcd329596f840e3a
Luigi Cerone <luigicerone96@gmail.com>
2019-08-24 11:45:15 +0200

    [add] Option in hamburger menu when logged in POST/PUT of section a1 information List of datetime update in section a1 Minor fixes

--- code/gui/src/app/pages/questionnaire/section-a1/section-a1.component.ts
+++ code/gui/src/app/pages/questionnaire/section-a1/section-a1.component.ts
@@ -17,7 +17,7 @@ export interface Options {
 })
 
 export class SectionA1Component implements OnInit {
-  public form: FormGroup;
+  private form: FormGroup;
 
   subjectOpt: Options[] = [
     {value: 'case', viewValue: 'Case'},
@@ -54,15 +54,15 @@ export class SectionA1Component implements OnInit {
   buildForm() {
     this.form = new FormGroup({
       subject: new FormControl(this.a1.subject , Validators.required),
-      database: new FormControl(this.a1.dbCodeNumber, [
+      dbCodeNumber: new FormControl(this.a1.dbCodeNumber, [
         Validators.required,
         Validators.pattern('[0-9]{4}[MC][0-9]{4}')
       ]),
-      date: new FormControl(this.a1.dateOfQuestionnaireAdministration, [
+      dateOfQuestionnaireAdministration: new FormControl(this.a1.dateOfQuestionnaireAdministration, [
         Validators.required,
         Validators.pattern('[0-9]{2}[/][A-Z]{1}[a-z]{2}[/][0-9]{4}')
       ]),
-      typeMelanoma: new FormControl(this.a1.typeOfMelanoma, Validators.required),
+      typeOfMelanoma: new FormControl(this.a1.typeOfMelanoma, Validators.required),
       otherSpecification: new FormControl(this.a1.otherSpecification)
     });
   }
@@ -73,12 +73,12 @@ export class SectionA1Component implements OnInit {
   }
 
   save() {
-    console.log(this.form);
+    this.a1 = new SectionA1(this.form);
 
-    // TODO
-    // this.questionnaireService.insertSection(this.authenticationService.currentUserValue.username, 'a1', this.form).subscribe( (res) => {
-    //   console.log(res);
-    // });
+    console.log(this.a1);
+    this.questionnaireService.insertSection(this.authenticationService.currentUserValue.username, 'a1', this.a1).subscribe( (res) => {
+      console.log(res);
+    });
   }
 
 }
--- code/rest/src/main/java/com/univaq/disim/bioinfo/service/SectionA1ServiceImpl.java
+++ code/rest/src/main/java/com/univaq/disim/bioinfo/service/SectionA1ServiceImpl.java
@@ -10,6 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 @Service
 public class SectionA1ServiceImpl implements SectionA1Service {
     @Autowired
@@ -22,12 +28,20 @@ public class SectionA1ServiceImpl implements SectionA1Service {
 
         if (q != null) {
             // There is an error, the user has already inserted a questionnaire.
-            throw new BusinessLayerException(HttpStatus.BAD_REQUEST, ErrorMessage.DB_CODE_NUMBER_ALREADY_IN_USE);
+//            throw new BusinessLayerException(HttpStatus.BAD_REQUEST, ErrorMessage.DB_CODE_NUMBER_ALREADY_IN_USE);
+            return this.update(username, a1);
 
         } else {
             q = new Questionnaire();
             q.setOwnerUsername(username);
+
+            if( a1.getDatesOfUpdateQuestionnaire() == null){
+                ArrayList<String> list = new ArrayList<String>(Collections.singleton(a1.getDateOfQuestionnaireAdministration()));
+                a1.setDatesOfUpdateQuestionnaire(list);
+            }
             q.setA1(a1);
+
+
             return questionnaireRepository.insert(q).getA1();
         }
     }
@@ -46,8 +60,22 @@ public class SectionA1ServiceImpl implements SectionA1Service {
     }
 
     @Override
-    public A1 update(String documentId, A1 a1) {
-        return null;
+    public A1 update(String username, A1 a1) throws BusinessLayerException {
+        Questionnaire q = questionnaireRepository.findOneByOwnerUsername(username);
+
+
+        if (q == null) {
+            // There is an error, the user has already inserted a questionnaire.
+            throw new BusinessLayerException(HttpStatus.NOT_FOUND, ErrorMessage.SECTION_MISSING);
+        }
+
+        String updateDate = DateTimeFormatter.ofPattern("dd/MMM/yyyy").format(LocalDate.now());
+        
+        a1.setDatesOfUpdateQuestionnaire(q.getA1().getDatesOfUpdateQuestionnaire());
+        a1.getDatesOfUpdateQuestionnaire().add(updateDate);
+        q.setA1(a1);
+
+        return questionnaireRepository.save(q).getA1();
     }
 
     @Override
